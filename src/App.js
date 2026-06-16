74431dee4c5b54750c3ac30773f26c590dea4732
Jorge Toro (Jolth) <jolthgs@gmail.com>
2026-06-16 17:13:39 -0500

    Nice Name

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Helmet>
         <meta
           name="description"
-          content="CoffeeIsGood is a free online tool that allows you to easily create timestamp links for YouTube videos. Share specific moments from videos with your friends and audience."
+          content="XXXXXX is a free online tool that allows you to easily create timestamp links for YouTube videos. Share specific moments from videos with your friends and audience."
         />
         <meta
           name="keywords"
